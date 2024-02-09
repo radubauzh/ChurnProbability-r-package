054b601c07439db52ecfc6323a349d53c2d7de58
Rafael <45404885+radubauzh@users.noreply.github.com>
2024-02-09 11:12:24 +0100

    Delete MyFinalPackage directory

--- MyFinalPackage/R/churnProbability.R
+++
@@ -1,8 +0,0 @@
-getChurnProbability <- function(dataset, customerId) {
-  if (!customerId %in% dataset$CustomerId) {
-    stop("Customer ID not found in the dataset")
-  }
-  return(dataset[CustomerId == customerId, ]$ChurnProbability)
-}
-
-usethis::use_testthat()
